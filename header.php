ae8cb7f11658357c76a6e2319ad77744a707baa7
Andrew Milne <andrewdouglasmilne@googlemail.com>
2014-07-28 16:38:00 +0100

    Included line to code to allow 18a website to track which sites have latest version of wordpress

--- header.php
+++ header.php
@@ -8,6 +8,10 @@
 			?>
 		</title>
 		<?php wp_head(); ?>
+
+		<!-- Allows 18a website check each install to make sure they are running the latest version -->
+		<meta name="generator" content="WordPress <?php echo bloginfo('version'); ?>" />
+		
 		<!--html5shiv allows you to use HTML 5 elements for versions of IE which do not recognise HTML 5 elements. See http://en.wikipedia.org/wiki/HTML5_Shiv for more details-->
 		<!--[if lt IE 9]><script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	</head>
